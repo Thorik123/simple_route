56f01de5641d0c474544ed41ba6f4ceb65dba1c0
Thorik123 <tronikthorik07@gmail.com>
2021-12-18 04:30:02 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+//import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Latihan Routname',
+    initialRoute: '/',
+    routes: <String, WidgetBuilder>{
+      '/': (context) => const HomePage(),
+      '/about': (context) => const AboutPage(),
+    },
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Belajar Routing'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/about');
+          },
+          child: const Text('Tap untuk About Page'),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tentang Aplikasi'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Kembali'),
+        ),
+      ),
+    );
+  }
+}
